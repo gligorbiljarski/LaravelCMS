252a338c55af1e4a5b220c7a7b287e27271dc665
gligorbiljarski <gbiljarski@icloud.com>
2023-02-22 19:41:44 +0100

    Adding Roles

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::middleware(['web', 'auth'])->prefix('dashboard')->group(function () {
 
     Route::resource('/users', App\Http\Controllers\UserController::class);
